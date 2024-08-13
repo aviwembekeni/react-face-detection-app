5bc022b1cb4374fc4c0f88bd260c48e1ede9493f
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-08-13 12:14:26 +0200

    changed heroku link to render api link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navigation  from "./components/Navigation/Navigation";
+import React, { Component } from "react";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import SignIn from "./components/SignIn/SignIn";
 import Register from "./components/Register/Register";
-import Particles from 'react-particles-js';
-import Clarifai from'clarifai';
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 
 const particlesOptions = {
   particles: {
@@ -16,126 +16,118 @@ const particlesOptions = {
       number: {
         value: 30,
         density: {
-          enable:true,
-          value_area: 800
-        }
-      }
-    }
-  }
-}
-
-
+          enable: true,
+          value_area: 800,
+        },
+      },
+    },
+  },
+};
 
 const intialState = {
-  input: '',
-  imgUrl: '',
+  input: "",
+  imgUrl: "",
   box: {},
-  route: 'signin',
+  route: "signin",
   isSignedIn: false,
   user: {
-      id: '',
-      name: '',
-      email: '',
-      entries: 0,
-      joined: ''
-  }
-}
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: "",
+  },
+};
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = intialState;
   }
 
   componentDidMount() {
-    fetch('https://face-recognition-app-av.herokuapp.com/')
-    .then(response => response.json())
-    .then(console.log)
-    .catch(err => console.log(err))
+    fetch("https://face-recognition-api-gpts.onrender.com/")
+      .then((response) => response.json())
+      .then(console.log)
+      .catch((err) => console.log(err));
   }
 
   displayFaceBox = (box) => {
-
-    this.setState({box: box});
-  }
+    this.setState({ box: box });
+  };
 
   calculateFaceLocation = (data) => {
-
-      const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-      const img = document.getElementById('inputImage');
-      const width = Number(img.width);
-      const height = Number(img.height);
-      return {
-        leftCol: clarifaiFace.left_col * width,
-        topRow: clarifaiFace.top_row * height,
-        rightCol: width - (clarifaiFace.right_col * width),
-        bottomRow: height - (clarifaiFace.bottom_row * height)
-      }
-
-  }
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const img = document.getElementById("inputImage");
+    const width = Number(img.width);
+    const height = Number(img.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
   onInputChange = (e) => {
-      this.setState({input: e.target.value})
-
-  }
+    this.setState({ input: e.target.value });
+  };
 
   onSubmit = () => {
+    this.setState({ imgUrl: this.state.input });
 
-    this.setState({imgUrl: this.state.input})
-
-      fetch('https://face-recognition-app-av.herokuapp.com/imageUrl', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          input: this.state.input
-        })
-      })
-      .then(response => response.json())
-      .then(response => {
+    fetch("https://face-recognition-api-gpts.onrender.com/imageUrl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
         if (response) {
-          fetch('https://face-recognition-app-av.herokuapp.com/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
+          fetch("https://face-recognition-api-gpts.onrender.com/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-              id: this.state.user.id
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
             })
-          })
-          .then(response => response.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, {entries: count}))
-          })
-          .catch( console.log)
+            .catch(console.log);
         }
-       this.displayFaceBox(this.calculateFaceLocation(response))
-
+        this.displayFaceBox(this.calculateFaceLocation(response));
       })
-      .catch(err => console.log("err", err))
-
-  }
+      .catch((err) => console.log("err", err));
+  };
 
   onRouteChange = (route) => {
-    if(route === 'signout'){
-      this.setState(intialState)
-    }else if(route === 'home'){
-      this.setState({isSignedIn: true})
+    if (route === "signout") {
+      this.setState(intialState);
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState({route: route})
-  }
+    this.setState({ route: route });
+  };
 
   loadUser = (data) => {
-
-     this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
-     }})
-
-  }
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   render() {
-    const {input, imgUrl, box, route, isSignedIn, user} = this.state;
+    const { input, imgUrl, box, route, isSignedIn, user } = this.state;
     return (
       <div className="App">
         {/*<Particles className="particles"
@@ -143,21 +135,28 @@ class App extends Component {
 
     />*/}
 
-        <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn}/>
-        { route === 'home'
-         ?
-         <div>
-          <Logo />
-          <Rank name={user.name} entries={user.entries}/>
-          <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
-          <FaceRecognition box={box} imgUrl={imgUrl}/>
-        </div>
-
-        : (route === 'signin')?
-        <SignIn onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
-        :
-        <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
-        }
+        <Navigation
+          onRouteChange={this.onRouteChange}
+          isSignedIn={isSignedIn}
+        />
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank name={user.name} entries={user.entries} />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onSubmit={this.onSubmit}
+            />
+            <FaceRecognition box={box} imgUrl={imgUrl} />
+          </div>
+        ) : route === "signin" ? (
+          <SignIn onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+        ) : (
+          <Register
+            onRouteChange={this.onRouteChange}
+            loadUser={this.loadUser}
+          />
+        )}
       </div>
     );
   }
